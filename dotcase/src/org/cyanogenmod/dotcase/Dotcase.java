6952052f31fc16e41ae621dd9ebc62193748ffb4
Patrik Zakhar <123patokt@gmail.com>
2016-04-29 21:20:38 +0200

    dotcase: disable heads up while dotcase is on
    Change-Id: Idca5f6e96e12719d18fec76f96c4c340174a1541

--- dotcase/src/org/cyanogenmod/dotcase/Dotcase.java
+++ dotcase/src/org/cyanogenmod/dotcase/Dotcase.java
@@ -37,6 +37,7 @@ import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.Display;
 import android.view.GestureDetector;
@@ -128,6 +129,8 @@ public class Dotcase extends Activity implements SensorEventListener
         mSensorManager.registerListener(this,
                 mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY),
                 SensorManager.SENSOR_DELAY_NORMAL);
+        Settings.Global.putInt(mContext.getContentResolver(),
+                Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED, 0);
     }
 
     @Override
@@ -138,6 +141,8 @@ public class Dotcase extends Activity implements SensorEventListener
         } catch (IllegalArgumentException e) {
             Log.e(TAG, "Failed to unregister listener", e);
         }
+        Settings.Global.putInt(mContext.getContentResolver(),
+                Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED, 1);
     }
 
     class Service implements Runnable {
